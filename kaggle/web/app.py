f70b6e9f9d69d9b5f91000f157f504ee6b66d0d6
dougc333 <dougc@stanfordalumni.org>
2017-06-17 20:43:44 -0700

    update

--- kaggle/web/app.py
+++ kaggle/web/app.py
@@ -1,10 +1,17 @@
+import jinja2
+import os
 from flask import Flask, render_template
-app = Flask(__name__,static_folder='../kaggle/train/Type_1')
+
+
+app = Flask(__name__)
+
+def init():
+    os.path.list
 
 
 @app.route("/")
 def main():
-    return render_template("index.html")
+    return render_template("index.html",img_list=["../static/train/Type_1/700.jpg"])
 
 if __name__=="__main__":
     app.run()

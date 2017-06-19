0106adac44daecda4ad6ae1a139eb7f297cd8b21
dougc333 <dougc@stanfordalumni.org>
2017-06-19 15:24:30 -0700

    add web

--- kaggle/web/app.py
+++ kaggle/web/app.py
@@ -1,22 +1,45 @@
 import jinja2
 import os
+import json
 from os import path,walk
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 import sqlite3
 from sqlite3 import connect
 
 app = Flask(__name__)
-list_type1 = []
 
-def init():
-    list_f=[]
-    print(os.getcwd())
+list_type1 = []
+list_type2 = []
+list_type3 = []
+
+def init_type1():
+    list_type1=[]
     for dirpath, dirnames, filenames in walk(os.getcwd()+"/static/train/Type_1"):
         for f in filenames:
             if f!='.DS_Store':
                 #print(f)
-                list_f.append (f)
-        return list_f
+                list_type1.append (f)
+        return list_type1
+
+def init_type2():
+    list_type2=[]
+    for dirpath, dirnames, filenames in walk(os.getcwd()+"/static/train/Type_2"):
+        for f in filenames:
+            if f!='.DS_Store':
+                #print(f)
+                list_type2.append (f)
+        return list_type2
+
+def init_type3():
+    list_type3=[]
+    for dirpath, dirnames, filenames in walk(os.getcwd()+"/static/train/Type_3"):
+        for f in filenames:
+            if f!='.DS_Store':
+                #print(f)
+                list_type3.append (f)
+        return list_type3
+
+
 
 def init_db():
     database_file = "images.db"
@@ -29,11 +52,50 @@ def init_db():
     for row in rows:
         print(row)
 
-@app.route("/")
-def main():
-    return render_template("index.html",img_list=list_type1,img_ht=100,img_width=100)
+
+
+@app.route('/test')
+def checklist():
+    row_values = [1, 2, 3, 4]
+    all_tags = []
+
+    for rownum in range(1,3):
+        all_tags.append(row_values[1])
+
+    return render_template('test.html', tags=all_tags)
+
+@app.route('/signUp')
+def signUp():
+    print ("signUp")
+    return render_template('signUp.html')
+
+
+@app.route('/signUpUser', methods=['POST'])
+def signUpUser():
+    print ("signUpUser")
+    user =  request.form['username'];
+    password = request.form['password'];
+    print('user:',user)
+    print('password:',password)
+    return json.dumps({'status':'OK','user':user,'pass':password});
+
+@app.route("/type1")
+def type1():
+    return render_template("type1.html",img_list1=list_type1,img_ht=100,img_width=100)
+
+@app.route("/type2")
+def type2():
+    return render_template("type2.html", img_list2=list_type2,img_ht=100,img_width=100)
+
+@app.route("/type3")
+def type3():
+    return render_template("type3.html", img_list3=list_type3,img_ht=100,img_width=100)
+
+
 
 if __name__=="__main__":
     init_db()
-    list_type1.extend(init())
+    list_type2.extend(init_type2())
+    list_type3.extend(init_type3())
+    list_type1.extend(init_type1())
     app.run()

8114d8d73c6a79f929963ecf45fe5768851ca838
dougc333 <dougc@stanfordalumni.org>
2017-11-06 15:43:18 -0800

    add

--- hinton/assignment3/a3.m
+++ hinton/assignment3/a3.m
@@ -1,6 +1,6 @@
 function a3(wd_coefficient, n_hid, n_iters, learning_rate, momentum_multiplier, do_early_stopping, mini_batch_size)
   warning('error', 'Octave:broadcast');
-  if exist('page_output_immediately'), page_output_immediately(1); end
+  #if exist('page_output_immediately'), page_output_immediately(1); end
   more off;
   model = initial_model(n_hid);
   from_data_file = load('data.mat');

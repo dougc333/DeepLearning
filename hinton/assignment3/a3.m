87870546c23338531f5e028e12d139e0eb7bb12b
dougc333 <dougc@stanfordalumni.org>
2017-11-16 07:20:26 -0800

    add

--- hinton/assignment3/a3.m
+++ hinton/assignment3/a3.m
@@ -1,13 +1,14 @@
 function a3(wd_coefficient, n_hid, n_iters, learning_rate, momentum_multiplier, do_early_stopping, mini_batch_size)
   warning('error', 'Octave:broadcast');
-  #if exist('page_output_immediately'), page_output_immediately(1); end
-  more on;
+  if exist('page_output_immediately'), 
+    page_output_immediately(1); 
+  end
+  %more off;
   model = initial_model(n_hid);
   from_data_file = load('data.mat');
   datas = from_data_file.data;
   n_training_cases = size(datas.training.inputs, 2);
-  if n_iters ~= 0,
-    fprintf('n_iters not zero calling test_gradient\n');
+  if n_iters ~= 0, 
     test_gradient(model, datas.training, wd_coefficient); 
   end
 
@@ -23,14 +24,18 @@ function a3(wd_coefficient, n_hid, n_iters, learning_rate, momentum_multiplier, 
   end
   for optimization_iteration_i = 1:n_iters,
     model = theta_to_model(theta);
-    fprintf('optimization_iteration_i:%d\n',optimization_iteration_i);
     training_batch_start = mod((optimization_iteration_i-1) * mini_batch_size, n_training_cases)+1;
+    fprintf('optimization_iteration_i:%d, training_batch_start:%d\n',optimization_iteration_i,training_batch_start);
     training_batch.inputs = datas.training.inputs(:, training_batch_start : training_batch_start + mini_batch_size - 1);
     training_batch.targets = datas.training.targets(:, training_batch_start : training_batch_start + mini_batch_size - 1);
+    fprintf('training_batch.inputs:%dx%d training_batch.targets:%dx%d\n',rows(training_batch.inputs),columns(training_batch.inputs),
+            rows(training_batch.targets),columns(training_batch.targets));
     gradient = model_to_theta(d_loss_by_d_model(model, training_batch, wd_coefficient));
+    fprintf('gradient %dx%d sum:%f\n',rows(gradient),columns(gradient),sum(sum(gradient)));
     momentum_speed = momentum_speed * momentum_multiplier - gradient;
     theta = theta + momentum_speed * learning_rate;
-
+    fprintf('theta %dx%d\n sum:%f\n', rows(theta),columns(theta),sum(sum(theta)));
+    
     model = theta_to_model(theta);
     training_data_losses = [training_data_losses, loss(model, datas.training, wd_coefficient)];
     validation_data_losses = [validation_data_losses, loss(model, datas.validation, wd_coefficient)];
@@ -43,30 +48,31 @@ function a3(wd_coefficient, n_hid, n_iters, learning_rate, momentum_multiplier, 
       fprintf('After %d optimization iterations, training data loss is %f, and validation data loss is %f\n', optimization_iteration_i, training_data_losses(end), validation_data_losses(end));
     end
   end
-  if n_iters ~= 0, test_gradient(model, datas.training, wd_coefficient); end % check again, this time with more typical parameters
+  if n_iters ~= 0, 
+    test_gradient(model, datas.training, wd_coefficient); 
+  end % check again, this time with more typical parameters
   if do_early_stopping,
-    #fprintf('Early stopping: validation loss was lowest after %d iterations. We chose the model that we had then.\n', best_so_far.after_n_iters);
+    fprintf('Early stopping: validation loss was lowest after %d iterations. We chose the model that we had then.\n', best_so_far.after_n_iters);
     theta = best_so_far.theta;
   end
   % the optimization is finished. Now do some reporting.
   model = theta_to_model(theta);
   if n_iters ~= 0,
-    clf;
-    hold on;
-    plot(training_data_losses, 'b');
-    plot(validation_data_losses, 'r');
-    legend('training', 'validation');
-    ylabel('loss');
-    xlabel('iteration number');
-    hold off;
+    #clf;
+    #hold on;
+    #plot(training_data_losses, 'b');
+    #plot(validation_data_losses, 'r');
+    #legend('training', 'validation');
+    #ylabel('loss');
+    #xlabel('iteration number');
+    #hold off;
   end
   datas2 = {datas.training, datas.validation, datas.test};
   data_names = {'training', 'validation', 'test'};
-  for data_i = 1:1,
-    fprintf('data_i:%d\n',data_i)
+  for data_i = 1:3,
     data = datas2{data_i};
     data_name = data_names{data_i};
-    #fprintf('learning_rate:%f momentum_multiplier:%f\n', learning_rate,momentum_multiplier)
+    fprintf('learning_rate:%f momentum_multiplier:%f\n', learning_rate,momentum_multiplier)
     fprintf('\nThe loss on the %s data is %f\n', data_name, loss(model, data, wd_coefficient));
     if wd_coefficient~=0,
       fprintf('The classification loss (i.e. without weight decay) on the %s data is %f\n', data_name, loss(model, data, 0));
@@ -76,30 +82,25 @@ function a3(wd_coefficient, n_hid, n_iters, learning_rate, momentum_multiplier, 
 end
 
 function test_gradient(model, data, wd_coefficient)
-  fprintf('test gradient\n');
   base_theta = model_to_theta(model);
   h = 1e-2;
   correctness_threshold = 1e-5;
   analytic_gradient = model_to_theta(d_loss_by_d_model(model, data, wd_coefficient));
-  
   % Test the gradient not for every element of theta, because that's a lot of work. Test for only a few elements.
-  %1:100
   for i = 1:100,
     test_index = mod(i * 1299721, size(base_theta,1)) + 1; % 1299721 is prime and thus ensures a somewhat random-like selection of indices
-    fprintf('test_index:%d\n',test_index);
     analytic_here = analytic_gradient(test_index);
     theta_step = base_theta * 0;
     theta_step(test_index) = h;
     contribution_distances = [-4:-1, 1:4];
     contribution_weights = [1/280, -4/105, 1/5, -4/5, 4/5, -1/5, 4/105, -1/280];
     temp = 0;
-    %1:8
     for contribution_index = 1:8,
       temp = temp + loss(theta_to_model(base_theta + theta_step * contribution_distances(contribution_index)), data, wd_coefficient) * contribution_weights(contribution_index);
     end
     fd_here = temp / h;
     diff = abs(analytic_here - fd_here);
-    #fprintf('%d %e %e %e %e\n', test_index, base_theta(test_index), diff, fd_here, analytic_here);
+    % fprintf('%d %e %e %e %e\n', test_index, base_theta(test_index), diff, fd_here, analytic_here);
     if diff < correctness_threshold, continue; end
     if diff / (abs(analytic_here) + abs(fd_here)) < correctness_threshold, continue; end
     error(sprintf('Theta element #%d, with value %e, has finite difference gradient %e but analytic gradient %e. That looks like an error.\n', test_index, base_theta(test_index), fd_here, analytic_here));
@@ -128,13 +129,7 @@ function ret = loss(model, data, wd_coefficient)
   hid_input = model.input_to_hid * data.inputs; % input to the hidden units, i.e. before the logistic. size: <number of hidden units> by <number of data cases>
   hid_output = logistic(hid_input); % output of the hidden units, i.e. after the logistic. size: <number of hidden units> by <number of data cases>
   class_input = model.hid_to_class * hid_output; % input to the components of the softmax. size: <number of classes, i.e. 10> by <number of data cases>
-  #fprintf('model.input_to_hid %dx%d\n', rows(model.input_to_hid),columns(model.input_to_hid));
-  #fprintf('loss model.input_to_hid sum:%f, sum data.inputs:%f\n', sum(sum(model.input_to_hid)),sum(sum(data.inputs)));
-  #fprintf('loss hid_input rows:%d cols:%d\n',rows(hid_input),columns(hid_input)); 
-  #fprintf('loss hid_output rows:%d cols:%d\n',rows(hid_output),columns(hid_output)); 
-  #fprintf('loss class_input rows:%d cols:%d\n',rows(class_input),columns(class_input)); 
-  #fprintf('loss data.inputs sum:%f\n', sum(sum(data.inputs)))
-  #fprintf('hid_input %f, hid_output:%f, class_input:%f\n',sum(sum(hid_input)),sum(sum(hid_output)),sum(sum(class_input)));
+  
   % The following three lines of code implement the softmax.
   % However, it's written differently from what the lectures say.
   % In the lectures, a softmax is described using an exponential divided by a sum of exponentials.
@@ -143,20 +138,12 @@ function ret = loss(model, data, wd_coefficient)
   % The exponential in the lectures can lead to really big numbers, which are fine in mathematical equations, but can lead to all sorts of problems in Octave.
   % Octave isn't well prepared to deal with really large numbers, like the number 10 to the power 1000. Computations with such numbers get unstable, so we avoid them.
   class_normalizer = log_sum_exp_over_rows(class_input); % log(sum(exp of class_input)) is what we subtract to get properly normalized log class probabilities. size: <1> by <number of data cases>
-  #fprintf('loss class_normalizer:%d x %d\n', rows(class_normalizer),columns(class_normalizer));
   log_class_prob = class_input - repmat(class_normalizer, [size(class_input, 1), 1]); % log of probability of each class. size: <number of classes, i.e. 10> by <number of data cases>
-  #fprintf('loss log_class_prob:%dx%d\n', rows(log_class_prob),columns(log_class_prob));
   class_prob = exp(log_class_prob); % probability of each class. Each column (i.e. each case) sums to 1. size: <number of classes, i.e. 10> by <number of data cases>
-  #fprintf('loss class_prob:%dx%d\n',rows(class_prob),columns(class_prob))
-  #fprintf('class_normalizer:%f, log_class_prob:%f, class_prob:%f\n',sum(class_normalizer),sum(sum(log_class_prob)),sum(sum(class_prob)));
+  
   classification_loss = -mean(sum(log_class_prob .* data.targets, 1)); % select the right log class probability using that sum; then take the mean over all data cases.
-  #fprintf("classification_loss:%f\n",classification_loss)
   wd_loss = sum(model_to_theta(model).^2)/2*wd_coefficient; % weight decay loss. very straightforward: E = 1/2 * wd_coeffecient * theta^2
-  #temp1 = model_to_theta(model).^2;
-  #temp2 = sum(model_to_theta(model).^2);
-  #fprintf('loss temp1:%f\n',temp1);
-  #fprintf('loss temp2:%f\n',temp2);
-  fprintf('loss wd_loss:%f\n', wd_loss);
+  #fprintf('wd_loss:%f\n', wd_loss);
   ret = classification_loss + wd_loss;
 end
 
@@ -165,11 +152,9 @@ function ret = d_loss_by_d_model(model, data, wd_coefficient)
   % model.hid_to_class is a matrix of size <number of classes i.e. 10> by <number of hidden units>
   % data.inputs is a matrix of size <number of inputs i.e. 256> by <number of data cases>. Each column describes a different data case. 
   % data.targets is a matrix of size <number of classes i.e. 10> by <number of data cases>. Each column describes a different data case. It contains a one-of-N encoding of the class, i.e. one element in every column is 1 and the others are 0.
-
-  % The returned object is supposed to be exactly like parameter <model>, i.e. it has fields ret.input_to_hid and ret.hid_to_class. However, the contents of those matrices are gradients (d loss by d model parameter), instead of model parameters.
-	 
+  % The returned object is supposed to be exactly like parameter <model>, i.e. it has fields ret.input_to_hid and ret.hid_to_class. However, the contents of those matrices are gradients (d loss by d model parameter), instead of model parameters.	 
   % This is the only function that you're expected to change. Right now, it just returns a lot of zeros, which is obviously not the correct output. Your job is to replace that by a correct computation.
-  
+
   m = size(data.inputs,2);
   hid_input = model.input_to_hid * data.inputs;
   hid_output = logistic(hid_input);
@@ -179,21 +164,14 @@ function ret = d_loss_by_d_model(model, data, wd_coefficient)
   class_output = exp(log_class_prob);
   
   delta_3 = class_output - data.targets;
-  temp = logistic(hid_input);
-  temp1 = 1 - logistic(hid_input);
-  temp2 = logistic(hid_input) .*(1-logistic(hid_input));
-  fprintf('d_loss_by_d_model: temp %dx%d, temp1 %dx%d, temp2 %dx%d\n', rows(temp),columns(temp),rows(temp1),columns(temp1),rows(temp2),columns(temp2))
   delta_2 = (model.hid_to_class' * delta_3) .* (logistic(hid_input) .*(1-logistic(hid_input)));
-  
   
   ret.input_to_hid = (1 / m) .* (delta_2 * data.inputs') + wd_coefficient.* model.input_to_hid;
   ret.hid_to_class =(1 / m) .* (delta_3 * hid_output') + wd_coefficient.* model.hid_to_class;
-  %ret.input_to_hid = (1 / n_training_cases) .* (delta_2 * data.inputs') + wd_coefficient.* model.input_to_hid;
-  %ret.hid_to_class =(1 / n_training_cases) .* (delta_3 * hid_output') + wd_coefficient.* model.hid_to_class;
-
+  
 end
 
-function ret = model_to_theta(model)
+function ret = model_to_-theta(model)
   % This function takes a model (or gradient in model form), and turns it into one long vector. See also theta_to_model.
   input_to_hid_transpose = transpose(model.input_to_hid);
   hid_to_class_transpose = transpose(model.hid_to_class);
@@ -203,7 +181,6 @@ end
 function ret = theta_to_model(theta)
   % This function takes a model (or gradient) in the form of one long vector (maybe produced by model_to_theta), and restores it to the structure format, i.e. with fields .input_to_hid and .hid_to_class, both matrices.
   n_hid = size(theta, 1) / (256+10);
-  #fprintf('theta_to_model n_hid:%d\n',n_hid);
   ret.input_to_hid = transpose(reshape(theta(1: 256*n_hid), 256, n_hid));
   ret.hid_to_class = reshape(theta(256 * n_hid + 1 : size(theta,1)), n_hid, 10).';
 end
@@ -211,11 +188,8 @@ end
 function ret = initial_model(n_hid)
   n_params = (256+10) * n_hid;
   as_row_vector = cos(0:(n_params-1));
-  #as_row_vector = (0:(n_params-1));
-  #fprintf('n_hid:%d n_params:%d as_row_vector:%dx%d numel:%d\n',n_hid, n_params, rows(as_row_vector),columns(as_row_vector),numel(as_row_vector));
   ret = theta_to_model(as_row_vector(:) * 0.1); % We don't use random initialization, for this assignment. This way, everybody will get the same results.
-  #ret = theta_to_model(as_row_vector(:));
- end
+end
 
 function ret = classification_performance(model, data)
   % This returns the fraction of data cases that is incorrectly classified by the model.
--- hinton/assignment3/a3old.m
+++ hinton/assignment3/a3old.m
@@ -1,12 +1,15 @@
 function a3(wd_coefficient, n_hid, n_iters, learning_rate, momentum_multiplier, do_early_stopping, mini_batch_size)
   warning('error', 'Octave:broadcast');
-  if exist('page_output_immediately'), page_output_immediately(1); end
-  %more off;
+  #if exist('page_output_immediately'), page_output_immediately(1); end
+  more on;
   model = initial_model(n_hid);
   from_data_file = load('data.mat');
   datas = from_data_file.data;
   n_training_cases = size(datas.training.inputs, 2);
-  if n_iters ~= 0, test_gradient(model, datas.training, wd_coefficient); end
+  if n_iters ~= 0,
+    fprintf('n_iters not zero calling test_gradient\n');
+    test_gradient(model, datas.training, wd_coefficient); 
+  end
 
   % optimization
   theta = model_to_theta(model);
@@ -20,8 +23,9 @@ function a3(wd_coefficient, n_hid, n_iters, learning_rate, momentum_multiplier, 
   end
   for optimization_iteration_i = 1:n_iters,
     model = theta_to_model(theta);
-    
+    #fprintf('optimization_iteration_i:%d\n',optimization_iteration_i);
     training_batch_start = mod((optimization_iteration_i-1) * mini_batch_size, n_training_cases)+1;
+    fprintf('training_batch_start:%d opt_iter:%d mini_batch_size:%d n_training_cases:%d\n', training_batch_start,optimization_iteration_i,mini_batch_size,n_training_cases);
     training_batch.inputs = datas.training.inputs(:, training_batch_start : training_batch_start + mini_batch_size - 1);
     training_batch.targets = datas.training.targets(:, training_batch_start : training_batch_start + mini_batch_size - 1);
     gradient = model_to_theta(d_loss_by_d_model(model, training_batch, wd_coefficient));
@@ -42,7 +46,7 @@ function a3(wd_coefficient, n_hid, n_iters, learning_rate, momentum_multiplier, 
   end
   if n_iters ~= 0, test_gradient(model, datas.training, wd_coefficient); end % check again, this time with more typical parameters
   if do_early_stopping,
-    fprintf('Early stopping: validation loss was lowest after %d iterations. We chose the model that we had then.\n', best_so_far.after_n_iters);
+    #fprintf('Early stopping: validation loss was lowest after %d iterations. We chose the model that we had then.\n', best_so_far.after_n_iters);
     theta = best_so_far.theta;
   end
   % the optimization is finished. Now do some reporting.
@@ -59,10 +63,11 @@ function a3(wd_coefficient, n_hid, n_iters, learning_rate, momentum_multiplier, 
   end
   datas2 = {datas.training, datas.validation, datas.test};
   data_names = {'training', 'validation', 'test'};
-  for data_i = 1:3,
+  for data_i = 1:1,
+    fprintf('data_i:%d\n',data_i)
     data = datas2{data_i};
     data_name = data_names{data_i};
-    fprintf('learning_rate:%f momentum_multiplier:%f\n', learning_rate,momentum_multiplier)
+    #fprintf('learning_rate:%f momentum_multiplier:%f\n', learning_rate,momentum_multiplier)
     fprintf('\nThe loss on the %s data is %f\n', data_name, loss(model, data, wd_coefficient));
     if wd_coefficient~=0,
       fprintf('The classification loss (i.e. without weight decay) on the %s data is %f\n', data_name, loss(model, data, 0));
@@ -72,25 +77,30 @@ function a3(wd_coefficient, n_hid, n_iters, learning_rate, momentum_multiplier, 
 end
 
 function test_gradient(model, data, wd_coefficient)
+  fprintf('test gradient\n');
   base_theta = model_to_theta(model);
   h = 1e-2;
   correctness_threshold = 1e-5;
   analytic_gradient = model_to_theta(d_loss_by_d_model(model, data, wd_coefficient));
+  
   % Test the gradient not for every element of theta, because that's a lot of work. Test for only a few elements.
+  %1:100
   for i = 1:100,
     test_index = mod(i * 1299721, size(base_theta,1)) + 1; % 1299721 is prime and thus ensures a somewhat random-like selection of indices
+    fprintf('test_index:%d\n',test_index);
     analytic_here = analytic_gradient(test_index);
     theta_step = base_theta * 0;
     theta_step(test_index) = h;
     contribution_distances = [-4:-1, 1:4];
     contribution_weights = [1/280, -4/105, 1/5, -4/5, 4/5, -1/5, 4/105, -1/280];
     temp = 0;
+    %1:8
     for contribution_index = 1:8,
       temp = temp + loss(theta_to_model(base_theta + theta_step * contribution_distances(contribution_index)), data, wd_coefficient) * contribution_weights(contribution_index);
     end
     fd_here = temp / h;
     diff = abs(analytic_here - fd_here);
-    % fprintf('%d %e %e %e %e\n', test_index, base_theta(test_index), diff, fd_here, analytic_here);
+    #fprintf('%d %e %e %e %e\n', test_index, base_theta(test_index), diff, fd_here, analytic_here);
     if diff < correctness_threshold, continue; end
     if diff / (abs(analytic_here) + abs(fd_here)) < correctness_threshold, continue; end
     error(sprintf('Theta element #%d, with value %e, has finite difference gradient %e but analytic gradient %e. That looks like an error.\n', test_index, base_theta(test_index), fd_here, analytic_here));
@@ -119,7 +129,7 @@ function ret = loss(model, data, wd_coefficient)
   hid_input = model.input_to_hid * data.inputs; % input to the hidden units, i.e. before the logistic. size: <number of hidden units> by <number of data cases>
   hid_output = logistic(hid_input); % output of the hidden units, i.e. after the logistic. size: <number of hidden units> by <number of data cases>
   class_input = model.hid_to_class * hid_output; % input to the components of the softmax. size: <number of classes, i.e. 10> by <number of data cases>
-  
+ 
   % The following three lines of code implement the softmax.
   % However, it's written differently from what the lectures say.
   % In the lectures, a softmax is described using an exponential divided by a sum of exponentials.
@@ -130,9 +140,9 @@ function ret = loss(model, data, wd_coefficient)
   class_normalizer = log_sum_exp_over_rows(class_input); % log(sum(exp of class_input)) is what we subtract to get properly normalized log class probabilities. size: <1> by <number of data cases>
   log_class_prob = class_input - repmat(class_normalizer, [size(class_input, 1), 1]); % log of probability of each class. size: <number of classes, i.e. 10> by <number of data cases>
   class_prob = exp(log_class_prob); % probability of each class. Each column (i.e. each case) sums to 1. size: <number of classes, i.e. 10> by <number of data cases>
-  
   classification_loss = -mean(sum(log_class_prob .* data.targets, 1)); % select the right log class probability using that sum; then take the mean over all data cases.
   wd_loss = sum(model_to_theta(model).^2)/2*wd_coefficient; % weight decay loss. very straightforward: E = 1/2 * wd_coeffecient * theta^2
+  #fprintf('loss wd_loss:%f\n', wd_loss);
   ret = classification_loss + wd_loss;
 end
 
@@ -145,7 +155,6 @@ function ret = d_loss_by_d_model(model, data, wd_coefficient)
   % The returned object is supposed to be exactly like parameter <model>, i.e. it has fields ret.input_to_hid and ret.hid_to_class. However, the contents of those matrices are gradients (d loss by d model parameter), instead of model parameters.
 	 
   % This is the only function that you're expected to change. Right now, it just returns a lot of zeros, which is obviously not the correct output. Your job is to replace that by a correct computation.
-<<<<<<< HEAD
   
   m = size(data.inputs,2);
   hid_input = model.input_to_hid * data.inputs;
@@ -156,15 +165,18 @@ function ret = d_loss_by_d_model(model, data, wd_coefficient)
   class_output = exp(log_class_prob);
   
   delta_3 = class_output - data.targets;
+  temp = logistic(hid_input);
+  temp1 = 1 - logistic(hid_input);
+  temp2 = logistic(hid_input) .*(1-logistic(hid_input));
+  fprintf('d_loss_by_d_model: temp %dx%d, temp1 %dx%d, temp2 %dx%d\n', rows(temp),columns(temp),rows(temp1),columns(temp1),rows(temp2),columns(temp2))
   delta_2 = (model.hid_to_class' * delta_3) .* (logistic(hid_input) .*(1-logistic(hid_input)));
   
   
   ret.input_to_hid = (1 / m) .* (delta_2 * data.inputs') + wd_coefficient.* model.input_to_hid;
   ret.hid_to_class =(1 / m) .* (delta_3 * hid_output') + wd_coefficient.* model.hid_to_class;
-=======
-  ret.input_to_hid = (1 / n_training_cases) .* (delta_2 * data.inputs') + wd_coefficient.* model.input_to_hid;
-  ret.hid_to_class =(1 / n_training_cases) .* (delta_3 * hid_output') + wd_coefficient.* model.hid_to_class;
->>>>>>> 6401ba01b42d58480f07371bceb3ede6fb55e533
+  %ret.input_to_hid = (1 / n_training_cases) .* (delta_2 * data.inputs') + wd_coefficient.* model.input_to_hid;
+  %ret.hid_to_class =(1 / n_training_cases) .* (delta_3 * hid_output') + wd_coefficient.* model.hid_to_class;
+
 end
 
 function ret = model_to_theta(model)
@@ -177,6 +189,7 @@ end
 function ret = theta_to_model(theta)
   % This function takes a model (or gradient) in the form of one long vector (maybe produced by model_to_theta), and restores it to the structure format, i.e. with fields .input_to_hid and .hid_to_class, both matrices.
   n_hid = size(theta, 1) / (256+10);
+  #fprintf('theta_to_model n_hid:%d\n',n_hid);
   ret.input_to_hid = transpose(reshape(theta(1: 256*n_hid), 256, n_hid));
   ret.hid_to_class = reshape(theta(256 * n_hid + 1 : size(theta,1)), n_hid, 10).';
 end
@@ -184,8 +197,11 @@ end
 function ret = initial_model(n_hid)
   n_params = (256+10) * n_hid;
   as_row_vector = cos(0:(n_params-1));
+  #as_row_vector = (0:(n_params-1));
+  #fprintf('n_hid:%d n_params:%d as_row_vector:%dx%d numel:%d\n',n_hid, n_params, rows(as_row_vector),columns(as_row_vector),numel(as_row_vector));
   ret = theta_to_model(as_row_vector(:) * 0.1); % We don't use random initialization, for this assignment. This way, everybody will get the same results.
-end
+  #ret = theta_to_model(as_row_vector(:));
+ end
 
 function ret = classification_performance(model, data)
   % This returns the fraction of data cases that is incorrectly classified by the model.

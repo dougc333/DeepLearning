ec132b12539bb3a654c1e9c78a74f319d3dbfd99
dougc333 <dougc@stanfordalumni.org>
2018-05-21 13:17:23 -0700

    add

--- twitter_nlp/setup.py
+++ twitter_nlp/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='twitter_nlp',
       version='0.1',
       description='ethan twitter stuff',
-      url='http://github.com/dougc333/DeepLearning/twitter_nlp',
+      url='http://github.com/dougc333/DeepLearning/twitter_nlp.zip',
       author='asdf',
       author_email='asdf@asdf.com',
       packages=['ethan']
